563ce6ef21f043e84b09728aad1cdc95d3c48727
Tom Elliott <tom.elliott@auckland.ac.nz>
2016-02-12 12:31:51 +1300

    added datasets

---
+++ R/datasets.R
@@ -0,0 +1,54 @@
+#' Sea Ice
+#'
+#' A dataset containing sea ice measurements from 1990 to 2011.
+#'
+#' @format A data frame with 265 rows and 3 variables:
+#' \describe{
+#'   \item{Time}{The time variable}
+#'   \item{Arctic}{Sea ice measurement for the Arctic}
+#'   \item{Antarctica}{Sea ice measurement for Antarctica}
+#' }
+"seaice"
+
+
+#' Visitors (annual)
+#'
+#' A dataset containing annual visitor numbers for several countries.
+#'
+#' @format A data frame with 13 rows and 5 variables:
+#' \describe{
+#'   \item{Time}{The time variable (year)}
+#'   \item{Australia}{Visitor counts for Australia}
+#'   \item{China..People.s.Republic.of}{Visitor counts for China}
+#'   \item{Japan}{Visitor counts for Japan}
+#'   \item{United.Kingdom}{Visitor counts for the UK}
+#' }
+"visitorsA2"
+
+#' Visitors (monthly)
+#'
+#' A dataset containing monthly visitor numbers for several countries.
+#'
+#' @format A data frame with 164 rows and 5 variables:
+#' \describe{
+#'   \item{Time}{The time variable (year/month)}
+#'   \item{Australia}{Visitor counts for Australia}
+#'   \item{China..People.s.Republic.of}{Visitor counts for China}
+#'   \item{Japan}{Visitor counts for Japan}
+#'   \item{United.Kingdom}{Visitor counts for the UK}
+#' }
+"visitorsM2"
+
+#' Visitors (quarterly)
+#'
+#' A dataset containing quarterly visitor numbers for several countries.
+#'
+#' @format A data frame with 54 rows and 5 variables:
+#' \describe{
+#'   \item{Time}{The time variable (year/quarter)}
+#'   \item{Australia}{Visitor counts for Australia}
+#'   \item{China..People.s.Republic.of}{Visitor counts for China}
+#'   \item{Japan}{Visitor counts for Japan}
+#'   \item{United.Kingdom}{Visitor counts for the UK}
+#' }
+"visitorsQ"
