4fb9dbff5b11ef3f07a21ea748401045c3f5800a
Tom Elliott <tomelliottnz@gmail.com>
2020-06-12 09:42:59 +1200

    Fix subsetting bug in detecting time info

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -3,8 +3,8 @@ context("Utility functions")
 test_that("Multiplicative series only if x are all positive", {
     expect_true(is_multiplicative(1:10, TRUE))
     expect_false(is_multiplicative(1:10, FALSE))
-    expect_false(is_multiplicative(-100:-50, TRUE))
-    expect_false(is_multiplicative(-50:50, TRUE))
+    expect_warning(expect_false(is_multiplicative(-100:-50, TRUE)))
+    expect_warning(expect_false(is_multiplicative(-50:50, TRUE)))
 })
 
 test_that("Leap year function returns the correct value", {
