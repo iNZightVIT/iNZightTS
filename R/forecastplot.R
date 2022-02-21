c1704a38142ac172de827cdc61f08530efced0cf
szmsu2011 <szmsu2011@hotmail.co.nz>
2022-02-21 19:36:27 +1300

    univariate plot method for inz_frct

--- R/forecastplot.R
+++ R/forecastplot.R
@@ -47,10 +47,11 @@ predict.inz_ts <- function(x, var = NULL, h = "2 years", mult_fit = FALSE,
         mult_fit <- !mult_fit
         rlang::warn("Non-positive obs detected, setting `mult_fit = FALSE`")
     }
-    var <- na.omit(dplyr::case_when(
-        length(var) < 3 ~ dplyr::last(as.character(var)),
-        TRUE ~ c(NA_real_, as.character(var)[-1])
-    ))
+    if (length(var) < 3) {
+        var <- dplyr::last(as.character(var))
+    } else {
+        var <- as.character(var)[-1]
+    }
 
     inzightts_forecast_ls <- lapply(var, function(y_var) {
         predict_inzightts_var(x, sym(y_var), h, mult_fit, pred_model, confint_width)
@@ -87,6 +88,79 @@ predict_inzightts_var <- function(x, var, h, mult_fit, pred_model, confint_width
 
 
 #' @export
-plot.inz_frct <- function(x) {
-    
+plot.inz_frct <- function(x, xlab = NULL, ylab = NULL, title = NULL, plot = TRUE) {
+    if (is.null(xlab)) {
+        xlab <- dplyr::case_when(
+            is.numeric(x$index) ~ "Year",
+            TRUE ~ stringr::str_to_title(class(x$index)[1])
+        )
+    }
+    x <- dplyr::rename(x, !!dplyr::first(xlab) := index)
+
+    if (is.null(ylab)) ylab <- unique(x$.var)
+
+    if (!isTRUE(all.equal(length(unique(x$.var)), length(ylab)))) {
+        paste0("ylab should be of length ", length(unique(x$.var)), ".") %>%
+            rlang::abort()
+    }
+    if (is.null(title)) {
+        title <- dplyr::case_when(
+            length(unique(x$.var)) > 1 ~ "",
+            TRUE ~ unique(x$.var)
+        )
+    }
+
+    if (length(unique(x$.var)) == 1) {
+        p <- plot_forecast_var(x, sym(unique(x$.var)), xlab, ylab, title)
+    } else {
+        p_ls <- lapply()
+    }
+
+    if (plot) print(p)
+
+    invisible(p)
+}
+
+
+plot_forecast_var <- function(x, var, xlab, ylab, title) {
+    x <- dplyr::filter(x, .var == as.character(var))
+
+    pred_data <- x %>%
+        dplyr::filter(.model == "Fitted") %>%
+        dplyr::filter(dplyr::row_number() == dplyr::n()) %>%
+        dplyr::bind_rows(dplyr::filter(x, .model == "Prediction")) %>%
+        dplyr::mutate(
+            .lower = tidyr::replace_na(.lower, .$.mean[1]),
+            .upper = tidyr::replace_na(.upper, .$.mean[1]),
+            .model = "Prediction"
+        )
+
+    l_spec <- aes(!!sym(xlab), .mean)
+
+    index_obs <- pred_data[[as.character(xlab)]]
+    if (lubridate::is.Date(index_obs) | inherits(index_obs, "vctrs_vctr")) {
+        vline_xintercept <- as.Date(index_obs)[1]
+    } else {
+        vline_xintercept <- as.numeric(index_obs)[1]
+    }
+
+    p <- x %>%
+        dplyr::select(-.var) %>%
+        dplyr::filter(.model != "Raw data") %>%
+        fabletools::autoplot(.mean) +
+        geom_ribbon(
+            mapping = aes(!!sym(xlab), ymin = .lower, ymax = .upper),
+            data = pred_data, fill = "#ffdbdb", col = NA
+        ) +
+        geom_line(l_spec, dplyr::filter(x, .model == "Raw data"), size = 1) +
+        geom_line(l_spec, pred_data) +
+        geom_line(aes(!!sym(xlab), .lower), pred_data, linetype = "dashed") +
+        geom_line(aes(!!sym(xlab), .upper), pred_data, linetype = "dashed") +
+        geom_vline(xintercept = vline_xintercept, linetype = "dashed", alpha = .8) +
+        scale_colour_manual(values = c("#0e8c07", "#b50000", "black")) +
+        ggplot2::labs(title = title, y = ylab) +
+        ggplot2::theme(
+            legend.position = "bottom",
+            legend.title = element_blank()
+        )
 }
