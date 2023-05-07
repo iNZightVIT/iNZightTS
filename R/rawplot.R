c809564e706cc1a9885efc56be56cac4ea39a939
szmsu2011 <szmsu2011@hotmail.co.nz>
2023-05-08 07:24:45 +1200

    use rlang injection and native pipe

--- R/rawplot.R
+++ R/rawplot.R
@@ -50,9 +50,9 @@ cat_y_axis <- function(x) {
         } else if (inherits(idx, "yearweek")) {
             fun <- function(x) lubridate::week(x)
         }
-        name <- is.numeric(idx) %>%
-            ifelse("", substring(class(idx)[1], 5)) %>%
-            stringr::str_to_title() %>%
+        name <- is.numeric(idx) |>
+            ifelse("", substring(class(idx)[1], 5)) |>
+            stringr::str_to_title() |>
             paste("of Year")
     }
     list(fun = fun, name = name)
@@ -125,14 +125,12 @@ plot.inz_ts <- function(x, var = NULL, xlab = NULL, ylab = NULL, title = NULL,
         is.factor(x[[v]]) | is.character(x[[v]])
     })))
     if (!show_iso_obs) iso_obs_size <- -1
-
     if (tsibble::n_keys(x) > 1) {
-        x <- x %>%
-            dplyr::mutate(dplyr::across(
-                !!tsibble::key_vars(.), function(x) {
-                    factor(x, levels = sort(unique(x)))
-                }
-            ))
+        x <- x |> dplyr::mutate(dplyr::across(
+            !!tsibble::key_vars(x), function(x) {
+                factor(x, levels = sort(unique(x)))
+            }
+        ))
     }
     if (var_has_cat) {
         if (var_has_num) {
@@ -142,12 +140,12 @@ plot.inz_ts <- function(x, var = NULL, xlab = NULL, ylab = NULL, title = NULL,
             var <- c("", dplyr::first(var[var != ""]))
             rlang::warn("Please specify one character/factor at a time.")
         }
-        x <- x %>% dplyr::mutate(
-            !!dplyr::last(var) := rlang::eval_tidy(rlang::new_quosure(expr(interaction(!!!({
-                c(lapply(tsibble::key_vars(.), function(i) .[[i]]), list(.[[dplyr::last(var)]]))
-            }), sep = "/"))))
+        x <- x |> dplyr::mutate(
+            !!dplyr::last(var) := rlang::inject(interaction(!!!({
+                c(lapply(tsibble::key_vars(x), function(i) x[[i]]), list(x[[dplyr::last(var)]]))
+            }), sep = "/"))
         )
-        if (tsibble::n_keys(x) > 1 & is.null(emphasise)) {
+        if (tsibble::n_keys(x) > 1 && is.null(emphasise)) {
             emphasise <- 1L
             rlang::warn("Key detected when plotting category, setting `emphasise = 1L`")
         }
@@ -158,24 +156,24 @@ plot.inz_ts <- function(x, var = NULL, xlab = NULL, ylab = NULL, title = NULL,
             c("", as.character(var)[-1]),
             dplyr::last(as.character(var))
         ), function(i) x[[i]]))
-        if (any(y_obs <= 0, na.rm = TRUE) & mult_fit) {
+        if (any(y_obs <= 0, na.rm = TRUE) && mult_fit) {
             mult_fit <- !mult_fit
             rlang::warn("Non-positive obs detected, setting `mult_fit = FALSE`")
         }
     }
-    if (!is.null(xlim) & !var_has_cat) {
+    if (!is.null(xlim) && !var_has_cat) {
         if (!all(length(xlim) == 2, any(is.numeric(xlim), inherits(xlim, "Date")))) {
             rlang::abort("xlim must be a numeric or Date vector of length 2.")
         }
         na_i <- which(is.na(xlim))[1]
-        if (!is.numeric(x[[tsibble::index_var(x)]]) & is.numeric(xlim)) {
+        if (!is.numeric(x[[tsibble::index_var(x)]]) && is.numeric(xlim)) {
             xlim[na_i] <- lubridate::year(dplyr::case_when(
                 as.logical(na_i - 1) ~ dplyr::last(x$index),
                 TRUE ~ x$index[1]
             ))
             xlim <- lubridate::ymd(paste0(xlim, c("0101", "1231")))
             x <- dplyr::filter(x, dplyr::between(lubridate::as_date(index), xlim[1], xlim[2]))
-        } else if (is.numeric(x[[tsibble::index_var(x)]]) & inherits(xlim, "Date")) {
+        } else if (is.numeric(x[[tsibble::index_var(x)]]) && inherits(xlim, "Date")) {
             xlim[na_i] <- lubridate::ymd(paste0(ifelse(na_i - 1, dplyr::last(x$index), x$index[1]), "0101"))
             x <- dplyr::filter(x, dplyr::between(index, lubridate::year(xlim[1]), lubridate::year(xlim[2])))
         } else {
@@ -193,7 +191,6 @@ plot.inz_ts <- function(x, var = NULL, xlab = NULL, ylab = NULL, title = NULL,
         )
     }
     x <- dplyr::rename(x, !!dplyr::first(xlab) := index)
-
     if (is.null(ylab)) {
         ylab <- as.character(var)
         if (length(var) > 1) ylab <- ylab[-1]
@@ -212,21 +209,19 @@ plot.inz_ts <- function(x, var = NULL, xlab = NULL, ylab = NULL, title = NULL,
     } else {
         emph <- NULL
     }
-
     if (length(var) > 2) {
         if (!isTRUE(all.equal(length(var) - 1, length(ylab)))) {
             rlang::abort("var and ylab should have the same length.")
         }
-        if (!is.null(aspect) | (!compare & tsibble::n_keys(x) > 1)) {
+        if (!is.null(aspect) || (!compare && tsibble::n_keys(x) > 1)) {
             rlang::warn("Aspect ratio is automatic for multi-series plot.")
         }
     }
-    if (!is.null(aspect) & var_has_cat) {
+    if (!is.null(aspect) && var_has_cat) {
         rlang::warn("Aspect ratio is automatic for categorical plot.")
     }
-
     (if (length(var) < 3) {
-        if (!compare & tsibble::n_keys(x) > 1) aspect <- NULL
+        if (!compare && tsibble::n_keys(x) > 1) aspect <- NULL
         var <- sym(dplyr::last(as.character(var)))
         plot_inzightts_var(
             x, var, xlab, ylab, title, aspect, emph, pal,
@@ -240,21 +235,19 @@ plot.inz_ts <- function(x, var = NULL, xlab = NULL, ylab = NULL, title = NULL,
                 compare, smoother, sm_model, mult_fit, iso_obs_size
             )
         })
-        expr(patchwork::wrap_plots(!!!p_ls)) %>%
-            rlang::new_quosure() %>%
-            rlang::eval_tidy() +
+        rlang::inject(patchwork::wrap_plots(!!!p_ls)) +
             patchwork::plot_layout(ncol = 1, guides = "collect") +
             patchwork::plot_annotation(title = title) &
             ggplot2::theme(legend.position = "bottom")
-    }) %>% structure(use.plotly = ggplotable(.))
+    }) |> (\(.) structure(., use.plotly = ggplotable(.)))()
 }
 
 
 plot_inzightts_var <- function(x, var, xlab, ylab, title, aspect, emph, pal,
                                compare, smoother, sm_model, mult_fit, iso) {
     if (!is.null(emph)) {
-        emph_data <- emph$data %>%
-            dplyr::left_join(x, by = tsibble::key_vars(x), multiple = "all") %>%
+        emph_data <- emph$data |>
+            dplyr::left_join(x, by = tsibble::key_vars(x), multiple = "all") |>
             tsibble::as_tsibble(
                 index = !!tsibble::index(x),
                 key = !!tsibble::key_vars(x)
@@ -262,27 +255,22 @@ plot_inzightts_var <- function(x, var, xlab, ylab, title, aspect, emph, pal,
         if (emph$opacity == 0) x <- emph_data
     }
     op <- ifelse(!is.null(emph), emph$opacity, 1)
-
-    if (is.factor(x[[var]]) | is.character(x[[var]])) {
+    if (is.factor(x[[var]]) || is.character(x[[var]])) {
         return(plot_cat_var(x, var, title, pal))
     }
-
     p <- fabletools::autoplot(x, !!var, linewidth = 1, alpha = op) +
         ggplot2::labs(y = ylab, title = title) +
         ggplot2::theme(
             legend.position = dplyr::case_when(compare ~ "bottom", TRUE ~ "none"),
             legend.title = element_blank()
         )
-
     if (!is.null(emph)) {
         p <- suppressMessages(p + scale_colour_discrete(drop = FALSE)) +
             geom_line(data = emph_data, linewidth = 1)
     }
-
-    if (!compare & tsibble::n_keys(x) > 1) {
+    if (!compare && tsibble::n_keys(x) > 1) {
         p <- p + facet_wrap(vars(!!!tsibble::key(x)), nrow = 1)
     }
-
     if (!is.null(aspect)) {
         y_var <- dplyr::last(as.character(var))
         p <- p + coord_fixed(
@@ -290,7 +278,6 @@ plot_inzightts_var <- function(x, var, xlab, ylab, title, aspect, emph, pal,
                 diff(range(x[[y_var]], na.rm = TRUE)) / aspect
         )
     }
-
     if (smoother) {
         if (tsibble::n_keys(x) > 1) {
             sm_data <- decomp_key(x, as.character(var), sm_model, mult_fit)
@@ -308,28 +295,22 @@ plot_inzightts_var <- function(x, var, xlab, ylab, title, aspect, emph, pal,
             smoother_spec <- c(smoother_spec, col = "red")
         }
         if (!all(is.na(sm_data$trend))) {
-            p <- expr(p + geom_line(!!!smoother_spec)) %>%
-                rlang::new_quosure() %>%
-                rlang::eval_tidy()
+            p <- rlang::inject(p + geom_line(!!!smoother_spec))
         } else {
             rlang::warn("Time gaps in all (key) levels, turning off smoothers.")
         }
         if (!is.null(emph)) {
-            emph_sm <- emph$data %>%
+            emph_sm <- emph$data |>
                 dplyr::left_join(sm_data, by = tsibble::key_vars(x), multiple = "all")
             emph_sm_spec <- within(smoother_spec, rm(alpha, data))
-            p <- expr(p + geom_line(data = emph_sm, !!!emph_sm_spec)) %>%
-                rlang::new_quosure() %>%
-                rlang::eval_tidy()
+            p <- rlang::inject(p + geom_line(data = emph_sm, !!!emph_sm_spec))
         }
     }
-
     iso_i <- which(diff(diff(is.na(c(NA, x[[var]])))) == 2)
-    if (iso != -1 & length(iso_i) > 0) {
+    if (iso != -1 && length(iso_i) > 0) {
         p <- p + geom_point(data = x[iso_i, ], size = iso / 2) +
             ggplot2::labs(caption = "Isolated observations are plotted as dots")
     }
-
     p
 }
 
@@ -338,29 +319,29 @@ plot_cat_var <- function(x, var, title, pal) {
     ini_row <- dplyr::select(x[1, ], !!var)
     idx <- tsibble::index(x)
     if (!is.numeric(x[[idx]])) {
-        floor_idx <- suppressWarnings(ini_row[[idx]] %>%
-            lubridate::year() %>%
-            as.character() %>%
+        floor_idx <- suppressWarnings(ini_row[[idx]] |>
+            lubridate::year() |>
+            as.character() |>
             getFromNamespace(class(x[[idx]])[1], "tsibble")())
         if (ini_row[[idx]] != floor_idx) {
             ini_row[[idx]] <- floor_idx
             ini_row[[var]] <- NA
-            x <- x %>%
-                tsibble::update_tsibble(key = NULL) %>%
-                dplyr::bind_rows(ini_row) %>%
+            x <- x |>
+                tsibble::update_tsibble(key = NULL) |>
+                dplyr::bind_rows(ini_row) |>
                 tsibble::fill_gaps()
         }
     }
-    p <- x %>%
+    p <- x |>
         dplyr::mutate(
             .x = forcats::fct_inorder(as.character(
-                cat_x_axis(.)$fun(!!tsibble::index(x))
+                cat_x_axis(x)$fun(!!tsibble::index(x))
             )),
             .y = forcats::fct_inorder(as.character(
-                cat_y_axis(.)$fun(!!tsibble::index(x))
-            )) %>% forcats::fct_rev()
-        ) %>%
-        dplyr::filter(!is.na(!!var)) %>%
+                cat_y_axis(x)$fun(!!tsibble::index(x))
+            )) |> forcats::fct_rev()
+        ) |>
+        dplyr::filter(!is.na(!!var)) |>
         ggplot(aes(.x, .y, col = !!var)) +
         geom_tile(aes(fill = after_scale(col))) +
         ggplot2::theme(
