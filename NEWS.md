8f6c1a423d0342a89be5567702f483436637e670
Tom Elliott <tomelliottnz@gmail.com>
2024-05-24 16:03:07 +1200

    eg code

--- NEWS.md
+++ NEWS.md
@@ -1,13 +1,16 @@
 # iNZightTS 2.0.0
 
-Large update with major refactoring of most/all plots and processes. Notably,
+This is a new package based off the original [iNZightTS](github.com/iNZightVIT/iNZightTS) package. Versioning will continue, as the long-term plan is to merge these changes into the original package once it is fully deprecated.
 
 - rebuild package with features from **tidyverts**
 - new: sub-series plot
 - enhanced: introduce key for multiple time series
 - enhanced: changed the default behaviour of multiple variables in time series plots
 - added readme with example usage (#56)
-- Add option to seasonally adjust the series before plotting (default plot)
+
+---
+
+**Previous versions of the original `iNZightTS` package are listed here.**
 
 # iNZightTS 1.5.9
 
--- R/model.R
+++ R/model.R
@@ -2,8 +2,12 @@
 fit_model <- function(data, x, method = c("arima", "ets"), ...) {
     method <- match.arg(method)
     fit <- switch(method,
-        "arima" = fabletools::model(data, fit = fable::ARIMA(rlang::enquo(x), ...)),
-        "ets" = fabletools::model(data, fit = fable::ETS(rlang::enquo(x), ...))
+        "arima" = fabletools::model(data,
+            fit = fable::ARIMA(!!rlang::enquo(x), ...)
+        ),
+        "ets" = fabletools::model(data,
+            fit = fable::ETS(!!rlang::enquo(x), ...)
+        )
     )
     attr(fit, "method") <- method
     attr(fit, "checked") <- FALSE
@@ -13,10 +17,42 @@ fit_model <- function(data, x, method = c("arima", "ets"), ...) {
 
 #' @export
 print.inzts_fit <- function(x, ...) {
-    cat("Model: ", attr(x, "method"), "\n")
-    print.default(x, ...)
+    cli::cli_h3("Model of { attr(x, 'response') }")
+    # print.default(x, ...)
 
     if (!attr(x, "checked")) {
-        cat("Warning: model has not been checked for assumptions.\n")
+        cli::cli_warn("Model has not been checked for assumptions.\n")
+    }
+}
+
+#' @export
+plot.inzts_fit <- function(x, ...) {
+    f <- x[[1]][[1]]
+    d <- dplyr::bind_cols(f$data, f$fit$est)
+
+    p <- ggplot2::autoplot(d) +
+        ggplot2::geom_line(
+            ggplot2::aes(y = .fitted),
+            col = "green4"
+        )
+
+    if (!attr(x, "checked")) {
+        p <- p + ggplot2::labs(
+            subtitle = "Model has not been checked for assumptions."
+        ) +
+            ggplot2::theme(
+                plot.subtitle = ggplot2::element_text(
+                    face = "italic",
+                    color = "red"
+                )
+            )
+    }
+
+    p
+}
+
+check <- function(x, ...) {
+    if (attr(x, "checked")) {
+        return(x)
     }
 }
